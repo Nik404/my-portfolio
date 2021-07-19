c73f59271e86b67e2e7d7577690eea4fd0046bc7
Nikhal Singh <nsingh2_be18@thapar.edu>
2021-07-19 23:25:06 +0530

    add contact page

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,19 @@
 function newtab(){
     window.open("https://github.com/Nik404/ML-Projects/tree/master/Credit%20Card%20Fraud%20Detection");
 }
+
+function ftab(){
+    window.open("https://www.facebook.com");
+}
+
+function wtab(){
+    window.open("https://www.whatsapp.com");
+}
+
+function gtab(){
+    window.open("https://www.github.com/Nik404");
+}
+
+function ltab(){
+    window.open("https://in.linkedin.com/in/nikhil-singh-536349137");
+}
