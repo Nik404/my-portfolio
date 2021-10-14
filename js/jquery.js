052cef51ee662113f02b20d077d0db79003192e5
Nik404 <44531098+Nik404@users.noreply.github.com>
2021-10-14 10:48:58 +0530

    udate js file

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,21 @@
-function newtab(){
+function newtab1(){
+    window.open("https://github.com/Nik404/Face-Recognition");
+}
+
+function newtab2(){
     window.open("https://github.com/Nik404/ML-Projects/tree/master/Credit%20Card%20Fraud%20Detection");
 }
+
+
+function newtab3(){
+    window.open("https://github.com/Nik404/LRU-Cache-Project");
+}
+
+function newtab4(){
+    window.open("https://github.com/Nik404/ML-Projects/tree/master/Movie%20Rating%20Prediction");
+}
+
+
 
 function ftab(){
     window.open("https://www.facebook.com");
